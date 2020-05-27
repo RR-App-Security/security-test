eaa05d9798fec4f90493da0a6885be99e73c44c1
samfayssal <66031543+samfayssal@users.noreply.github.com>
2020-05-27 14:16:06 -0700

    Update test.php

--- test.php
+++ test.php
@@ -1,9 +1,10 @@
 <?php
+#include "sam.php"
 
 /* this code is to test SAST products */
 
 $v1 = 23;
 $v2 = 80;
 
-echo This is a test code: ' . $v1. "\n\r";
+echo "This is a test code: ' . $v1. "\n\r";
 ?>
